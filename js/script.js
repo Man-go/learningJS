9d7c7dbfc5eddf13df4dafbf54310e83c19f8efb
Severin <drevokocour01@gmail.com>
2021-02-27 22:25:35 +0100

    new fuctionality

--- js/script.js
+++ js/script.js
@@ -10,12 +10,34 @@ const personalMovieDB =  {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''), 
-      b = +prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''), 
+//           b = prompt('На сколько оцените его?');
+    
+//     if (a == '' || b == '')
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     personalMovieDB.movies[a] = b;
+// }
 
-console.log(personalMovieDB.movies);
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+    confirm("Просмотрено довольно мало фильмов", '');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    confirm("Вы классический зритель", '');
+} else if (personalMovieDB.count >= 30) {
+    confirm("Вы киноман", '');
+} else {
+    confirm("Произошла ошибка", '');
+}
+
+let i = 0;
+while (i < numberOfFilms) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?');
+
+    if (a !== '' || b !== '' || a.length < 50 || a !== null || b !== null) {
+        personalMovieDB.movies[a] = b;
+        i++;
+    } 
+}
+
+console.log(personalMovieDB);
